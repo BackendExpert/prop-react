9d05303c1431d41b1fed7b81ebf17035b9afec78
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 16:08:28 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,9 +1,12 @@
 import SignupIn from "../../templates/signupIn"
+import img from "../../assets/react.svg"
 
 const Login = () => {
   return (
     <div>
-        
+        <SignupIn src={img}>
+            <h1 className="text-3xl text-red-800 text-center">Login here</h1>
+        </SignupIn>
     </div>
   )
 }
