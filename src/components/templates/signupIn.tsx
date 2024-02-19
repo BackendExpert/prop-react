a2e5b007f228e222ddccbd96f1c443a48fb1485a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 16:04:42 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/templates/signupIn.tsx
+++ src/components/templates/signupIn.tsx
@@ -8,7 +8,8 @@ interface Props{
 const signupIn = ({children, src}: Props) => {
   return (
     <div className="container mx-auto px-32 py-20">
-        <img src={src} alt="" />
+        <img src={src} alt="" /><br /><br />
+        <div className="text-2xl"></div>
     </div>
   )
 }
