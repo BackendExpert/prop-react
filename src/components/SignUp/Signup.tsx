e75ca0a591bb7195e830905d2a38d2df6aaebaab
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 16:13:09 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/SignUp/Signup.tsx
+++ src/components/SignUp/Signup.tsx
@@ -1,8 +1,12 @@
+import SignupIn from "../../templates/signupIn"
+import img from "../../assets/react.svg"
 
 const Signup = () => {
   return (
     <div className="container mx-auto px-32 py-20">
-        
+        <SignupIn src={img}>
+            <h1 className="text-3xl text-red-800 text-center">Welcome to site</h1>
+        </SignupIn>
     </div>
   )
 }
--- src/templates/signupIn.tsx
+++ src/templates/signupIn.tsx
@@ -9,7 +9,7 @@ const signupIn = ({children, src}: Props) => {
   return (
     <div className="container mx-auto px-32 py-20">
         <img src={src} alt="" /><br /><br />
-        <div className="text-2xl">
+        <div className="text-2xl text-red-800 font-bold">
             {children}
         </div>
     </div>
